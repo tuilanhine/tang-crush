85fdaa42201c356d281a62eddf0951e2c40c6fe6
anhduc210 <72877434+anhduc210@users.noreply.github.com>
2021-06-20 15:36:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'hí em, lần đầu gặp em a đã muốn !',
+    introDesc: `Cất em vào trong bọc 
+   Không để ai nhòm ngó
+    Vì em là bảo ngọc 
+    Nên ai cũng muốn có  <3`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    desc: 'Anh thực sự thích em ý, làm người yêu anh nhá',
+    btnYes: 'Có, yêu anh hihi',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
